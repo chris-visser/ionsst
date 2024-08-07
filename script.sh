4f99e36ba9f93f58e846b75d8c35ab700e298f22
Chris Visser <2496739+chris-visser@users.noreply.github.com>
2024-08-07 09:58:32 +0200

    More basic base image

--- script.sh
+++ script.sh
@@ -1,2 +1,8 @@
-sst deploy --stage development --verbose
+cd ~/.pulumi/plugins
+ls -la
+
+# /root/.config/sst/bin/pulumi plugin install resource aws v6.45.0
+
+
+# sst deploy --stage development --verbose
 
---
+++ sst.config.ts
@@ -0,0 +1,15 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+    app() {
+      return {
+        home: 'aws',
+        name: 'dummy',
+        removal: 'retain',
+      }
+    },
+    async run() {
+
+    },
+  })
+  
