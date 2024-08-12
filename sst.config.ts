69b70302f1bd485802b45f3c655ba2b20a7e4b4e
Chris Visser <2496739+chris-visser@users.noreply.github.com>
2024-08-12 16:15:33 +0200

    Try

--- sst.config.ts
+++ sst.config.ts
@@ -1,15 +1,24 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
 export default $config({
-    app() {
-      return {
-        home: 'aws',
-        name: 'dummy',
-        removal: 'retain',
-      }
-    },
-    async run() {
+  app() {
+    return {
+      home: 'aws',
+      name: 'bc-fe-sst-poc',
+      removal: 'retain',
+    }
+  },
+  async run() {
+    const TestFunction = new sst.aws.Function('TestFunction', {
+      handler: 'index.handler',
+      role: 'arn:aws:iam::174382187256:role/bc-euc1-dev-aws-lambda-python-template',
+      url: true,
+    })
 
-    },
-  })
-  
+    new sst.aws.Router('PocRouter', {
+      routes: {
+        '/': TestFunction.url,
+      },
+    })
+  },
+})
